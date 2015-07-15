d68f70e95e771a8c4017772f65b093afe8ebf4ba
ParkGwangBeom <yellomobile@yellomobileui-iMac.local>
2015-07-15 16:34:19 +0900

    commit
    first

--- PikiScrollAnimation/ViewController.m
+++ PikiScrollAnimation/ViewController.m
@@ -9,10 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *v_Bottom;
 
 @end
 
 @implementation ViewController
+
+-(void)SetAnimation{
+    
+    [UIView animateWithDuration:0.5f animations:^{
+        self.v_Bottom.frame = CGRectMake(0.0f,
+                                         self.v_Bottom.frame.origin.y,
+                                         self.v_Bottom.frame.size.width,
+                                         self.v_Bottom.frame.size.height);
+    }completion:^(BOOL finish){
+        
+    }];
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self SetAnimation];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
